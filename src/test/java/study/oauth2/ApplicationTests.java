4cf8bdb6f5bfc4e7aaf9eb0535854739c0122b58
jun9898 <nice1998@gmail.com>
2024-07-01 14:36:51 +0900

    project init

---
+++ src/test/java/study/oauth2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
