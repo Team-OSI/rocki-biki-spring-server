82138a01797243900c54e2ecd5af8a541c11d32b
jun9898 <nice1998@gmail.com>
2024-07-05 20:03:45 +0900

    Feat: 요청 테스트용 testcontroller 추가

--- src/main/java/study/oauth2/user/controller/HelloController.java
+++ src/main/java/study/oauth2/user/controller/HelloController.java
@@ -1,9 +1,13 @@
 package study.oauth2.user.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
 	@GetMapping("/hello")
